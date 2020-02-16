55eaf97b4260fbcbeead360e6d8c2e7d1d729ab9
Weslley Richard <weslleyrc@pm.me>
2020-02-16 15:41:10 -0300

    Server Express + MongoDB Driver

---
+++ server/routes/api/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+// Get Posts
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
+});
+
+// Add Post
+router.post('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date(),
+  });
+
+  res.status(201).send();
+});
+
+router.delete('/:id', async (req, res) => {
+  const posts = await loadPostsCollection();
+  await posts.deleteOne({ _id: new mongodb.ObjectID(req.params.id) });
+
+  res.status(200).send();
+});
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(
+    'mongodb+srv://teste:teste@testes-ytqoz.gcp.mongodb.net/test?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useUnifiedTopology: true },
+  );
+
+  return client.db('Testes').collection('posts');
+}
+
+module.exports = router;
