776e43e010b1589fe1630c4971380c89ea2ea8c6
Weslley Richard <weslleyrc@pm.me>
2020-02-16 22:24:23 -0300

    Proxy + Build

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
+// Handle Production
+if (process.env.NODE_ENV === 'production') {
+  // Handle Static
+  app.use(express.static(__dirname + '/public/'));
+
+  // Handle SPA
+  app.use(/.*/, (req, res) => res.sendfile(__dirname + '/public/index.html'));
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Servidor iniciado na porta: ${port}`));
